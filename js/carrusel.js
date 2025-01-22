6d91ad22483508f28cbc6b89d83dec04101a3833
Andres Santiago Orozco Gorjon <santiogor15@gmail.com>
2025-01-22 13:04:04 -0600

    Mover json del carrusel a carpeta data

--- js/carrusel.js
+++ js/carrusel.js
@@ -90,7 +90,7 @@ function loadCarouselItems(jsonData) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('/carrusel.json')
+    fetch('/data/carrusel.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Failed to load carousel JSON');
